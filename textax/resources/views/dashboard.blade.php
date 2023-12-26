423229e7be0a84ac69dfda2fb5bd65493cad89d3
edi <edixshabani@gmail.com>
2023-12-26 03:56:42 +0100

    commit

--- textax/resources/views/dashboard.blade.php
+++ textax/resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex justify-center">
+            {{ __('Welcome to TexTax! An interactive and minimalistic discussing platform regarding latest news about technologies!') }}
         </h2>
     </x-slot>
 
