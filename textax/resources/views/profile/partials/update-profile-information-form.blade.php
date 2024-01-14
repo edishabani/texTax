801b786b82c6c9b22eff5892badb1af9c57d1e00
edi <edixshabani@gmail.com>
2024-01-14 01:10:01 +0100

    commit

--- textax/resources/views/profile/partials/update-profile-information-form.blade.php
+++ textax/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -2,9 +2,6 @@
 
 <section class="min-h-screen flex items-center justify-center bg-gray-90">
     <header class="flex bg-white p-6 rounded shadow">
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-400 flex">
-            {{ __('Profile Information') }}
-        </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __("Update your account's profile information and email address.") }}
